d58b3c0ec9493ba6f07a3363856ef48bcb58aa1f
Satish Kumar Chaudhary <skc05415518@gmail.com>
2025-04-27 13:34:22 +0545

    read,single read,delete,create done

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const connectToDatabase = require('./database')
-const Blog = require('./model/blogModel')
+const Blog = require('./model/blogModel') 
 const app = express()
 app.use(express.json())
+
+// access storage file 
+app.use(express.static('./storage'))
+
+// file system
+const fs = require('fs')
+
+// handle image and store 
+const {multer, storage} =require('./middleware/multerConfig')
+const upload = multer({storage : storage})
 
 // database connection here 
 connectToDatabase()
@@ -23,19 +33,77 @@ app.get("/about",(re,res)=>{
 })
 
 // create post api
-app.post("/blog",async(req,res)=>{
-    const {title,subtitle,description,image} = req.body
+app.post("/blog",upload.single('image'),async(req,res)=>{
+    const {title,subtitle,description} = req.body
+    const filename = req.file.filename
+    if(!title || !subtitle || !description){
+        return res.status(404).json({
+           message : "Please Provide title,subtitle,description"
+        })
+    }
     await Blog.create({
          Title : title,
          SubTitle: subtitle,
          Description: description,
-         Image: image
+         Image: filename
     })
     
     res.status(200).json({
-        message: "Blog Api Hit Successfully"
+        message: "Blog Created Successfully"
     })
 }) 
+
+// blogs details API
+app.get('/blog',async(req,res)=>{
+    const blogs =await Blog.find()
+    res.json({
+        message: "Blogs Details",
+        data : blogs
+
+    })
+}) 
+
+// read single blog API
+app.get('/blog/:id',async(req,res)=>{
+    const id = req.params.id
+    const singleBlog =await Blog.findById(id)
+    if(!singleBlog){
+        return res.status(400).json({
+            message : "No Data Found"
+        })
+    }
+    res.json({
+        message: "single blog details",
+        data : singleBlog
+    })
+})
+
+// Delete Blog API
+app.delete('/blog/:id',async (req,res)=>{
+    const id =req.params.id
+    const data = await Blog.findById(id)
+    const filename = data.Image
+    fs.unlink('storage/'+filename,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("File Deleted Successfully")
+        }
+    })
+    await Blog.findByIdAndDelete(id)
+    res.status(200).json({
+        message : "Blog Deteted Successfully"
+    })
+})
+
+// edit blog api 
+// app.patch('/post/:id',(req,res)=>{
+//     const id = req.params.id
+//     res.json({
+//         message : "Blog Updated Successfully"
+//     })
+// })
+
 
 
 
